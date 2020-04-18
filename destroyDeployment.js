203159b9115523932aab566d6763d6ff87394644
ksraj123 <kumarsaurabhraj.sr@gmail.com>
2020-04-18 18:44:00 +0530

    functional destroy

--- destroyDeployment.js
+++ destroyDeployment.js
@@ -3,6 +3,7 @@
 var Ansible = require('node-ansible');
 
 module.exports = function destroyDeployment (cluster, school, project, database, school_id){
+    database.collection('requests').updateOne({schoolId: school_id}, {$set: {status: 0}})
     var config = {
         project: process.env.PROJECT_ID || project.project_Id,
         zone: process.env.PROJECT_ZONE || project.zone,
@@ -11,26 +12,11 @@ module.exports = function destroyDeployment (cluster, school, project, database,
     }
     var database = database;
     var school_id = school_id;
-    var command = new Ansible.Playbook().playbook('./ansible/destroy-school-deployment.yml').variables(config);
+    var command = new Ansible.Playbook().playbook('./ansible/destroy-school-deployment').variables(config);
     var promise = command.exec();
     promise.then(function(result){
         // playbook sucessfully finished execution
-        var getJson = new Ansible.Playbook().playbook('./ansible/get-external-ip').variables(config);
-        getJson.exec().then(function(jsonRes){
-            // external IP is allocated after a few minutes of successful installation
-            // so few minutes pause added to external ip playbook
-            var res = jsonRes.output.slice(jsonRes.output.indexOf('{'), jsonRes.output.lastIndexOf('}')+1);
-            res = JSON.parse(JSON.parse(res).json.stdout_lines.reduce((acc, curr) => acc+curr, ""));
-            var externalIp = res.status.loadBalancer.ingress[0].ip;
-            console.log(`External Ip = ${externalIp}`);
-            database.collection('requests').updateOne({schoolId: school_id}, {$set: {status: 1, externalIp: externalIp, replicas: 3}}, 
-                function(err, result){
-                    if (err){
-                        console.log(err);
-                    }});
-        }).catch(function(err){
-            console.log(err);
-        })
+        database.collection('requests').deleteOne({schoolId: school_id})
     }).catch(err => {
         console.log(err);
     })
